49e873d1574f2a59e27ca06791fe6e75dafc11d0
Arnaud Leymet <arnaud.leymet@gmail.com>
2023-03-14 09:29:15 +0100

    Draft: Integration tests suite + Fix .zip collisions (#63)
    * :white_check_mark: Integration tests suite
    
    * :bug: Fix collision of .zip features
    
    * :memo: Fix gzip documentation
    
    ---------
    
    Co-authored-by: Arnaud Leymet <aleymet@bouyguestelecom.fr>

---
+++ test/integration/httpyac.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  cookieJarEnabled: false,
+  environments: {
+    $shared: {
+      host: "http://localhost:45540",
+      without_autoindex: "http://localhost:45541",
+      without_zip_directories: "http://localhost:45542",
+      without_meta_queries: "http://localhost:45543",
+    },
+  },
+};
