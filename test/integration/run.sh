ad90742b0c8b73ca7e34477ad0aa58ac50d659f6
qsahal <qsahal@bouyguestelecom.fr>
2023-12-23 20:25:46 +0100

    fix ci

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-npx httpyac@6.2 ./tests/*.http --silent --json --all -e dev >> ./test-output.json
+npx httpyac@6.2 ./tests/*.http --silent --json --all -e dev > ./test-output.json
 cat ./test-output.json | jq -r '.summary | "Total: \(.totalTests), Passed: \(.successTests), Failed: \(.failedTests)"'
 echo "Tests results details will be available under artifact http-yac-test-results"
+
+hasFailedTests=$(cat ./test-output.json | jq -r '.summary.failedTests')
+
+if [ $hasFailedTests -gt 0 ]; then
+  exit 1
+fi
