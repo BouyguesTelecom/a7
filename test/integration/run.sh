e079bf4db247011ded336e2800600096995d5b31
qsahal <qsahal@bouyguestelecom.fr>
2023-12-27 11:19:30 +0100

    small fix

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -2,7 +2,7 @@
 
 npx httpyac@6.2 ./tests/*.http --silent --json --all -e dev > ./test-output.json
 cat ./test-output.json | jq -r '.summary | "Total: \(.totalTests), Passed: \(.successTests), Failed: \(.failedTests)"'
-echo "Tests results details will be available under artifact `http-yac-test-results`"
+echo "Tests results details will be available under artifact 'http-yac-test-results'"
 
 hasFailedTests=$(cat ./test-output.json | jq -r '.summary.failedTests')
 
